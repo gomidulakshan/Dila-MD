eae15f221af360971aa621e9cf9657de308ef3f7
gomidulakshan <gomidulakshan@gmail.com>
2024-10-24 20:18:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "fKYWxQLA#O36W0I3_B7tLp9LUx0mM51w7GPJ0ZhReaTm9kPukVN0",
 MONGODB: process.env.MONGODB || "",
 };
